857fbdf1a65713a97eb8943b7b01cbc48926b415
Juan esteban molano <juanestebanmolanog@gmail.com>
2024-10-09 16:33:32 -0500

    Se crea la clase car y el formulario

---
+++ EjemploFundamentosCShart/Proyecto2/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+
+namespace Proyecto2
+{
+    public class Car 
+    {   
+        //Atributos de carro
+        string color;
+        int cilindraje;
+        int modelo;
+
+        //Metodo 
+        public string stop() {
+            string msj = "El carro esta detenido";
+            return msj;
+        
+        }
+    }
+}
---
+++ EjemploFundamentosCShart/Proyecto2/Index.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proyecto2
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //invocar 
+            mostrarMensaje();
+        }
+        public void mostrarMensaje() 
+        { 
+            Car car = new Car();
+            string msj = car.stop();
+            lblMensaje.Text = msj;
+        }
+    }
+}
